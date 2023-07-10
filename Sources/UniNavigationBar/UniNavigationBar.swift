9c004a1061c6e9b69cea0354175d69c9cbc09a18
ryuchanghwi <ckdgnl10@naver.com>
2023-07-10 16:48:06 +0900

    수정

--- Sources/UniNavigationBar/UniNavigationBar.swift
+++ Sources/UniNavigationBar/UniNavigationBar.swift
@@ -4,10 +4,12 @@ public class UniNavigationBar: UIView {
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
+        setStyle()
     }
     
     public required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setStyle()
     }
     
     private func setStyle() {
