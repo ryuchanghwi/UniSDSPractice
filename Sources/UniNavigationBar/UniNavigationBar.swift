6cbe16e303389e8bf1254b7bef236bae8fdbacd7
ryuchanghwi <ckdgnl10@naver.com>
2023-07-10 17:26:54 +0900

    NavigationBar 수정

--- Sources/UniNavigationBar/UniNavigationBar.swift
+++ Sources/UniNavigationBar/UniNavigationBar.swift
@@ -2,6 +2,22 @@ import UIKit
 
 public class UniNavigationBar: UIView {
     
+    let backButton: UIButton = {
+        let button = UIButton()
+        if #available(iOS 13.0, *) {
+            button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        } else {
+            // Fallback on earlier versions
+        }
+        return button
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        return label
+    }()
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
         setStyle()
@@ -12,7 +28,22 @@ public class UniNavigationBar: UIView {
         setStyle()
     }
     
-    private func setStyle() {
-        backgroundColor = .red
+    func setStyle() {
+        
+        [backButton, titleLabel].forEach {
+            addSubview($0)
+        }
+        
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        backButton.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        backButton.heightAnchor.constraint(equalToConstant: 24)
+            .isActive = true
+        backButton.widthAnchor.constraint(equalToConstant: 24)
+            .isActive = true
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: backButton.trailingAnchor, constant: 16).isActive = true
     }
 }
